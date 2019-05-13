5fed3629c17fb52384afe9e1334887bf97230749
intelaashish <aashish73134@gmail.com>
2019-05-12 23:56:56 -0700

    Working DDPG on Bipedal Walker, Mountain Car,Countinuous  CartPole

--- Continuous_Control_using_DDPG/Bipedal_Walker_using_DDPG.py
+++ Continuous_Control_using_DDPG/Bipedal_Walker_using_DDPG.py
@@ -1,32 +1,21 @@
 import gym
-import numpy as np
-import argparse
-from tensorboardX import SummaryWriter
-
-import numpy as np
-import time
 import torch
 from ddpg import DDPG
-from normalized_actions import NormalizedActions
-from ou_noise import OUNoise
-from parameter_noise import Adaptive_Parameter_Noise, ddpg_distance_metric
+from normalized_actions import NormalizedActions#----------------------------by default [-1,1] so do not need this class here
+from ounoise import OUNoise
+from param_noise import Adaptive_Parameter_Noise, ddpg_distance_metric
 from replay_memory import ReplayMemory, Transition
 from parameters import Parameters
-
-
+import Continuous_Cartpole
 args = Parameters()
-env_name = 'MountainCarContinuous-v0'
-env = NormalizedActions(gym.make(env_name))
-#env = gym.make(env_name)
 
+env_name = "BipedalWalker-v2"#----------Comment this line and uncomment the next line to see another environment
+#env_name = 'MountainCarContinuous-v0'#
+env = gym.make(env_name)
 
-writer = SummaryWriter()
-
-# env.seed(args.seed) Sets the seed for this env's random number generator(s).
-# torch.manual_seed(args.seed) You just need to call torch.manual_seed(seed), and it will set the seed of the random number generator to a fixed value,
-# so that when you call for example torch.rand(2), the results will be reproducible.
-# np.random.seed(args.seed)
-
+#Comment the above lines and uncomment the following if you want to see results faster
+# env_name = "Continuous Cartpole"
+# env = Continuous_Cartpole.ContinuousCartPoleEnv()
 
 agent = DDPG(args.gamma, args.tau, args.actor_hidden_size, env.observation_space.shape[0], env.action_space)
 
@@ -37,7 +26,6 @@ ounoise = OUNoise(env.action_space.shape[
 
 param_noise = Adaptive_Parameter_Noise(initial_action_stddev=0.05, desired_action_stddev=args.noise_scale,
                                        adaptation_coefficient=1.05) if args.param_noise else None
-# note that the initial_Action_stddev is in terms of parameter space but the desired action std is in terms of the action space
 
 rewards_train = []
 rewards_test = []
@@ -45,14 +33,11 @@ total_numsteps = 0
 global_total_no_of_updates = 0
 
 
-
-
 # ============================================Training
 for i_episode in range(args.num_episodes):
     print("Episode No:",i_episode)
     total_numsteps = 0
-    state = torch.Tensor(
-        [env.reset()])  # -----------------------reset the environment and get the default starting state
+    state = torch.Tensor([env.reset()])  # -----------------------reset the environment and get the default starting state
 
     if args.ou_noise:  # ----------------------------------------if OU noise enabled
         ounoise.scale = (args.noise_scale - args.final_noise_scale) * max(0, args.exploration_end -
@@ -60,27 +45,26 @@ for i_episode in range(args.num_episodes):
         ounoise.reset()
 
     if args.param_noise:  # --------------if parameter noise enabled, add noise to the actor's parameters
+
         agent.perturb_actor_parameters(param_noise)
 
     episode_reward = 0  # ----------------reward for the episode
 
     while True:  # -----------------------run the episode until we break by getting done = True after reaching the terminal state
 
-        action = agent.select_action(state, ounoise,
-                                     param_noise)  # ------------------------>select action using the learning actor
-        next_state, reward, done, _ = env.step(
-            action.numpy()[0])  # ------------------------>returns done value. used by mask as mask = - done,
+        action = agent.select_action(state, ounoise, param_noise)  # ------------------------>select action using the learning actor
+
+        next_state, reward, done, _ = env.step(action.cpu().numpy()[0])  # ------------------------>returns done value. used by mask as mask = - done,
 
 
         # if next state returned is a terminal state then return done = True, hence mask becomes 0  hence V(state before terminal state) = reward + mask * some value
-
+        env.render()
         total_numsteps += 1
         #print("timestep in the episode: ",total_numsteps)
         episode_reward += reward
 
-        action = torch.Tensor(action)  # --------------------------convert to Tensor
-        mask = torch.Tensor([
-                                not done])  # ------------------------mask is used to make sure that we multiply all the future rewards by 0 at the terminal state
+        action = torch.Tensor(action.cpu())  # --------------------------convert to Tensor
+        mask = torch.Tensor([ not done])  # ------------------------mask is used to make sure that we multiply all the future rewards by 0 at the terminal state
         next_state = torch.Tensor([next_state])
         reward = torch.Tensor([reward])
 
@@ -105,17 +89,13 @@ for i_episode in range(args.num_episodes):
 
                 value_loss, policy_loss = agent.update_parameters(batch)  # ------------>update_parameters() is getting a batch of transitions, returns two loss values
 
-                writer.add_scalar('loss/value', value_loss,
-                                  global_total_no_of_updates)  # add_scalar(tag, scalar_value, global_step=None, walltime=None)
-                writer.add_scalar('loss/policy', policy_loss, global_total_no_of_updates)
 
                 global_total_no_of_updates += 1
         if done:  # ------------------->if done == True, then break the while loop. Done is the end of this one single action from the actor n/w. we reach next state.
             break
 
-    writer.add_scalar('reward/train', episode_reward, i_episode)
 
-    # Adapting the param_noise based on distance metric after each episode
+    # Adapt the param_noise based on distance metric after each episode
 
     if args.param_noise:
         episode_transitions = replay_buffer.memory_list[replay_buffer.position - total_numsteps:replay_buffer.position]
@@ -123,39 +103,40 @@ for i_episode in range(args.num_episodes):
         unperturbed_actions = agent.select_action(states, None, None)
         perturbed_actions = torch.cat([transition[1] for transition in episode_transitions], 0)
 
-        ddpg_dist = ddpg_distance_metric(perturbed_actions.numpy(), unperturbed_actions.numpy())
+        ddpg_dist = ddpg_distance_metric(perturbed_actions.cpu().numpy(), unperturbed_actions.cpu().numpy())
         param_noise.adapt(ddpg_dist)
 
     rewards_train.append(episode_reward)
 
-    # ==============================================Testing after every 10 episodes
-    if i_episode % 10 == 0:
-        state = torch.Tensor([env.reset()])
-        episode_reward = 0
-        while True:
-            action = agent.select_action(state)
-
-            next_state, reward, done, _ = env.step(action.numpy()[0])
-            #env.render()#--------------------------------------------------removing render to run on the star server
-            episode_reward += reward
-
-            next_state = torch.Tensor([next_state])
-
-            state = next_state
-            if done:
-                break
-
-        writer.add_scalar('reward/test', episode_reward, i_episode)
-
-        rewards_test.append(episode_reward)
-        # Note that this is within this if condition.
-        print(
-            "Current Episode No: {}, Total numsteps in the last training episode: {}, Testing reward after the last training episode: {}, "
-            "Average training reward for the last ten training episodes: {}".format(i_episode, total_numsteps,
-                                                                                    rewards_test[-1],
-                                                                                    np.mean(rewards_train[-10:])))
+    # ==============================================Testing after every 10 episodes#---------->Removed in this code because here we just wanna see it
+    #learn---so I rendered all training episodes as well instead of just this testing episode
+    # if i_episode % 10 == 0:
+    #     state = torch.Tensor([env.reset()])
+    #     episode_reward = 0
+    #     while True:
+    #         action = agent.select_action(state)
+    #
+    #         next_state, reward, done, _ = env.step(action.cpu().numpy()[0])
+    #         #env.render()#--------------------------------------------------removing render to run on the star server
+    #         episode_reward += reward
+    #
+    #         next_state = torch.Tensor([next_state])
+    #
+    #         state = next_state
+    #         if done:
+    #             break
+    #
+    #     # writer.add_scalar('reward/test', episode_reward, i_episode)
+    #
+    #     rewards_test.append(episode_reward)
+    #     # Note that this is within this if condition.
+    #     print(
+    #         "Current Episode No: {}, Total numsteps in the last training episode: {}, Testing reward after the last training episode: {}, "
+    #         "Average training reward for the last ten training episodes: {}".format(i_episode, total_numsteps,
+    #                                                                                 rewards_test[-1],
+    #                                                                                 np.mean(rewards_train[-10:])))
 
 # save the actor and the policy that you get after all the episodes
 
-agent.save_all_episodes_model(env_name)
+#agent.save_all_episodes_model(env_name) # ------------>  can call this at some point to save the model
 env.close()
